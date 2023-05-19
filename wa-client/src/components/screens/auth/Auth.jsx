4ddca3063cc01d77ccd768d705e121fb613d2a04
Evgeniy <nikolaev_zheka-zik@bk.ru>
2023-05-19 12:12:48 +0300

    adds authorization

--- wa-client/src/components/screens/auth/Auth.jsx
+++ wa-client/src/components/screens/auth/Auth.jsx
@@ -6,31 +6,42 @@ import Field from "../../ui/field/Field"
 import Loader from "../../ui/loader/Loader"
 import styles from './Auth.module.scss'
 import { useState } from "react"
+import { useMutation } from "@tanstack/react-query"
+import authService from "../../../services/auth.service"
 
-const isLoading = false
-const isLoadingAuth = false
 
 const Auth = () => {
 
-    const [type, setType] = useState('auth')
+    const [type, setType] = useState('login')
 
     const {
         register, 
         handleSubmit, 
-        formState: {errors}
+        formState: {errors},
+        reset
     } = useForm({
         mode: 'onChange'
     })
 
+    const {mutate, isLoading} = useMutation(
+        ['auth'],
+        ({email, password}) => authService.main(email, password, type),
+        {
+            onSuccess: data => {
+                alert('success') 
+                reset()}
+            }
+        )
+
     const onSubmit = (data) => {
-        console.log(data, type)
+        mutate(data)
     }
 
     return (
         <>
             <Layout heading="Sign in" bgImage='/images/home-bg.jpg'/>
             <div className="wrapper-inner-page">
-                {(isLoading || isLoadingAuth) && <Loader/>}
+                {isLoading && <Loader/>}
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <Field
                         error={errors?.email?.message}
@@ -55,8 +66,8 @@ const Auth = () => {
 
                     />
                     <div className={styles.wrapperButtons}>
-                        <Button clickHandler={() => setType('auth')}>Sign in</Button>
-                        <Button clickHandler={() => setType('reg')}>Sign up</Button>
+                        <Button clickHandler={() => setType('login')}>Sign in</Button>
+                        <Button clickHandler={() => setType('register')}>Sign up</Button>
                     </div>
                 </form>
             </div>
--- wa-client/src/main.jsx
+++ wa-client/src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/index.scss'
 import Router from './routes/Routes'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router/>
+    <QueryClientProvider client={queryClient}>
+      <Router/>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
